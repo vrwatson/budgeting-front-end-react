13802c77f54739369e4c29a54f137ea93fe21ccc
vrwatson <vanessawatson@pursuit.org>
2021-06-21 17:36:39 -0400

    heroku update

--- react-budget-frontend/src/util/apiURL.js
+++ react-budget-frontend/src/util/apiURL.js
@@ -2,5 +2,5 @@ export const apiURL = () => {
     if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
         return "http://localhost:3333"
     }
-    return "";
+    return "https://powerful-hollows-13507.herokuapp.com";
 }
